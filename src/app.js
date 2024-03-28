4dd04c0a31ad89ade6ceb8fe850e977d51b4c1bd
Ashish Jadhav <ashishjadhav6923@gmail.com>
2024-03-28 21:49:13 +0530

    new api token

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const url = "https://api.clashofclans.com/v1/"
-const bearerToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImFiM2RiYjI4LTI5ODQtNGNiMi04NzFlLTdjYjI2NTNiODg0ZSIsImlhdCI6MTcxMDQwMjYyOCwic3ViIjoiZGV2ZWxvcGVyLzkzNWFmOTllLTdjNDMtN2U0MS1jOGFmLWM3MzczNjA3ZGZjNCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwMy4zNy4yMi4zNiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.tTB9yYd-9qeCbGyGJ_FqGMeCOJOUmINg7x4BlU_veXCxjJKJ5GZfcmGMx_bmQ4o0D4f1xNHVouPpaa_p_psWGg";
+const bearerToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZlMzRmNmI2LTRlNTItNGUwOC04YWM4LTk0YzJiNGEwMTc2MCIsImlhdCI6MTcxMTYxMTQ5Nywic3ViIjoiZGV2ZWxvcGVyLzkzNWFmOTllLTdjNDMtN2U0MS1jOGFmLWM3MzczNjA3ZGZjNCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjIyMy4yMzMuODcuMjA0Il0sInR5cGUiOiJjbGllbnQifV19.Ws59WUYpHECrT_HYDywaV98zOBMoJl_E2LMbHlL5fjyhgTJefo18yrACUQsc_xUoC0lhBkmBzXZUE4uiC6fEHA";
 const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,6 +31,7 @@ app.post("/players", async (req, res) => {
         res.render("index.ejs", { data: result, dataobj: response.data,type:type });
     } catch (error) {
         // res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+        console.log(error.response.data);
     }
 })
 app.post("/clans", async (req, res) => {
@@ -48,6 +49,7 @@ app.post("/clans", async (req, res) => {
         res.render("index.ejs", { data: result, dataobj: response.data,type:type });
     } catch (error) {
         // res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+        console.log(error.response.data);
     }
 })
 
